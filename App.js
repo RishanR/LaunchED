f6b10aedb9592820b12847bd4af6d360c7c468cf
JethoDennis <36519933+JethoDennis@users.noreply.github.com>
2018-10-22 22:26:46 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import React from "react";
+import { Avatar, Header, Text, Divider, Button, Icon } from "react-native-elements";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import Main from "./src/pages/Main.js";
 
-import { Login }  from "./src/pages/Login";
-import { Signup } from "./src/pages/Signup";
-import { AccountCredentials } from "./src/components/AccountCredentials";
 
 export default class App extends React.Component {
   render() {
-    return(
-    <View style={main.background}>
-      <LaunchedStackNavigator />
-    </View>
+    return (
+      <View style={{ backgroundColor: "#303030" }}>
+        <Main />
+      </View>
     );
   }
 }
 
-const LaunchedStackNavigator =  createStackNavigator({
-  LoginScreen: { screen: Login },
-  SignupScreen: { screen: Signup },
-})
-
-const main = StyleSheet.create({
-background: {
-  flex: 1,
-  backgroundColor:'#263238',
-},
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "grey"
+  },
+  titles: {
+    backgroundColor: "#202020",
+    paddingVertical: 5,
+    color: "#fff",
+    fontSize: 20,
+    fontFamily: "Montserrat",
+    fontWeight: "bold"
+  }
 });
